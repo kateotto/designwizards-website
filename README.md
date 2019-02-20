e03cbf21abf10463bce2522c42bf6d23c8ead9a1
Katie Otto <33523737+kateotto@users.noreply.github.com>
2019-02-20 19:36:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# designwizards-website" 
---
+++ script.js
@@ -0,0 +1,12 @@
+function info1() {
+    document.getElementById("info_holder").innerHTML = "Maecenas tristique felis nisi. Sed bibendum neque finibus, semper odio at, bibendum leo. Donec tincidunt dolor at iaculis consequat. Mauris eu commodo ex. Donec ac magna interdum, lacinia nunc sed, sagittis erat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi eu molestie erat, quis egestas lacus. Suspendisse luctus dui vel scelerisque feugiat. Quisque non eleifend orci. Nullam ut dolor et dui porttitor imperdiet ac vehicula elit.";
+}
+function info2() {
+    document.getElementById("info_holder").innerHTML = "Pellentesque vel ligula laoreet, bibendum risus eget, vestibulum libero. Cras sem justo, commodo vel auctor non, venenatis eu dui. Vestibulum elementum, dui nec efficitur laoreet, dui mi maximus velit, quis volutpat metus massa a lacus. Sed porttitor faucibus ipsum, nec auctor ante efficitur vel. Sed at elit quis diam rhoncus efficitur eu vel lorem.";
+}
+function info3() {
+    document.getElementById("info_holder").innerHTML = "Vivamus venenatis diam at lobortis sodales. Pellentesque dapibus, ex vitae facilisis faucibus, justo est facilisis sapien, eget maximus lectus arcu sed massa.";
+}
+function info4() {
+    document.getElementById("info_holder").innerHTML = "Praesent libero mi, consequat nec justo vitae, vehicula maximus diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur convallis purus eu leo malesuada vulputate a auctor diam.";
+}
